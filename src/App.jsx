cb758bfd3c592a641f498d5d6cec76354095ab47
tokuyama shouta <toku1031ts@gmail.com>
2020-11-13 17:22:13 +0900

    props state useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ColorfulMessage from './components/ColofulMessage';
+
 
 const App = () => {
+  const onClickCountUp = () => {
+    setNum(num +1);
+  };
+  const [num, setNum] = useState(0);
   return(
     <>
       <h1>こんにちは！</h1>
-      <p>お元気ですか</p>
+      <ColorfulMessage message={"よ！"}/>
+      <ColorfulMessage message={"よ！"}/>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   )
 }
---
+++ src/components/ColofulMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ColorfulMessage = (props) => {
+  return(
+      <p>{props.message}</p>
+  );
+}
+
+export default ColorfulMessage
