0d1e82b5bd28a93ed32d912897693f048ad63561
tokuyama shouta <toku1031ts@gmail.com>
2020-11-13 17:51:52 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import React, { useState } from 'react'
+
+import React, { useEffect, useState } from 'react'
 import ColorfulMessage from './components/ColofulMessage';
 
 
 const App = () => {
+
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num +1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  }
+
+  useEffect(() => {
+
+    if(num % 3 === 0){
+      faceShowFlag || setFaceShowFlag(true);
+    }else{
+      faceShowFlag && setFaceShowFlag(false)
+    }
+    // eslint-disable-next-line
+  }, [num])
+
+  
+
   return(
     <>
       <h1>こんにちは！</h1>
       <ColorfulMessage message={"よ！"}/>
       <ColorfulMessage message={"よ！"}/>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>（ ^ω^ ）</p>}
+      
     </>
   )
 }
